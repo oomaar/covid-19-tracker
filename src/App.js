cf2d7a79e303ff29b4fc2a65747503c8b4c877cc
Omar <omaar_5@hotmail.com>
2021-03-31 14:38:17 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <h1>
+      Let's Get Started
+    </h1>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { GlobalStyle } from './GlobalStyle';
+import App from './App';
+
+render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
