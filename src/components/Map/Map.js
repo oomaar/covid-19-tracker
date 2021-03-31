78aec16e90c88e0c5e85f24c57352b7731f8467f
Omar <omaar_5@hotmail.com>
2021-03-31 17:25:11 +0200

    setup map component

---
+++ src/components/Map/Map.js
@@ -0,0 +1,13 @@
+import {
+
+} from "./styledMap";
+
+const Map = () => {
+    return (
+        <h1>
+            MAP Component
+        </h1>
+    );
+};
+
+export default Map;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Header } from "./Header/Header";
 export { default as Stats } from "./Stats/Stats";
+export { default as Map } from "./Map/Map";
