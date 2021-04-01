492aad1289e321210cd0ba16dd1bdcd4c3a93024
Omar <omaar_5@hotmail.com>
2021-04-01 16:00:38 +0200

    complete stats component

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -4,14 +4,17 @@ import {
     Cases,
 } from "./styledStats";
 
-const Stats = ({ title, cases, totalCases }) => {
+const Stats = ({ isRed, active, title, cases, totalCases, ...restProps }) => {
     return (
         <Container>
-            <Card className="infoBox">
+            <Card
+                className={`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"}`}
+                onClick={restProps.onClick}
+            >
                 <CardContent>
                     <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
                     <Cases>{cases}</Cases>
-                    <Typography className="infoBox__cases">{totalCases} Total</Typography>
+                    <Typography className="infoBox__total">{totalCases} Total</Typography>
                 </CardContent>
             </Card>
         </Container>
--- src/components/Stats/styledStats.js
+++ src/components/Stats/styledStats.js
@@ -2,17 +2,33 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     .infoBox {
+        flex: 1;
 
+        :not(:last-child) {
+            margin-right: 15px;
+        }
     }
 
-    .infoBox__title {
-
+    .infoBox__total {
+        color: #6c757d;
+        font-weight: 700 !important;
+        font-size: 0.8rem !important;
+        margin-top: 15px !important;
     }
 
-    .infoBox__cases {
+    .infoBox--selected {
+        border-top: 10px solid greenyellow;
+    }
     
+    .infoBox--red {
+        border-color: #cc1034;
     }
 
 `;
 
-export const Cases = styled.h2``;
+export const Cases = styled.h2`
+        color: #cc1034;
+        font-weight: 600;
+        font-size: 1.75rem;
+        margin-bottom: 0.5rem;
+`;
